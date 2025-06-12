8901d6c3559d00f5cf9454c5c57c01b6b22e0d49
Genzer <732937+Genzer@users.noreply.github.com>
2025-06-12 13:53:13 +0700

    Collect starred GitHub everyday

--- scripts/fetch_stars.sh
+++ scripts/fetch_stars.sh
@@ -4,6 +4,11 @@ set -e -u -o pipefail
 main() {
   local -r github_handle="$1"
   local -r github_url="https://api.github.com/users/$github_handle/starred?per_page=1000"
+
+  # Print CSV Header
+  echo '"repo","url","description"'
+
+  # Print rows
   local page=1
   while curl -s "$github_url&page=${page}" \
     | jq -r -e '.[] | [.full_name, .html_url, .description] | @csv' && [[ ${PIPESTATUS[1]} != 4 ]]; do
