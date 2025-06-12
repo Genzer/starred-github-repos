394eacf5551029bf46037d336f6b388e95d8e9e5
Genzer <732937+Genzer@users.noreply.github.com>
2025-06-12 11:54:11 +0700

    Add my own starred repos

---
+++ scripts/fetch_stars.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e -u -o pipefail
+
+main() {
+  local -r github_handle="$1"
+  local -r github_url="https://api.github.com/users/$github_handle/starred?per_page=1000"
+  local page=1
+  while curl -s "$github_url&page=${page}" \
+    | jq -r -e '.[] | [.full_name, .html_url, .description] | @csv' && [[ ${PIPESTATUS[1]} != 4 ]]; do
+    let page++
+  done
+}
+
+main "$@"
