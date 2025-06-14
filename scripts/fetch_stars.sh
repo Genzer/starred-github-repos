f018cff2b45917d7043ce77cd2ab7f093756220e
Genzer Hawker <732937+Genzer@users.noreply.github.com>
2025-06-14 10:20:23 +0700

    Fail the script if HTTP response is not 200.
    Without using `curl --fail..`, the script empties the CSV when GitHub call fails.

--- scripts/fetch_stars.sh
+++ scripts/fetch_stars.sh
@@ -2,6 +2,11 @@
 set -e -u -o pipefail
 
 main() {
+
+  if [[ "$DEBUG" == "TRUE" ]]; then
+    set -x
+  fi
+
   local -r github_handle="$1"
   local -r github_url="https://api.github.com/users/$github_handle/starred?per_page=1000"
 
@@ -10,7 +15,7 @@ main() {
 
   # Print rows
   local page=1
-  while curl -s "$github_url&page=${page}" \
+  while curl -s --fail --show-error "$github_url&page=${page}" \
     | jq -r -e '.[] | [.full_name, .html_url, .description] | @csv' && [[ ${PIPESTATUS[1]} != 4 ]]; do
     let page++
   done
