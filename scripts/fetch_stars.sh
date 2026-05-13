6c860b64553ec67b7ebd4eca73ef906ea6ec0bec
Genzer <732937+Genzer@users.noreply.github.com>
2026-05-13 15:08:15 +0700

    Use GitHub CLI and application/vnd.github.v3.star+json
    The MIME type application/vnd.github.v3.star+json includes the timestamp
    when I starred the repo (i.e: starred_at). Use GitHub CLI for better
    pagination handling.

--- scripts/fetch_stars.sh
+++ scripts/fetch_stars.sh
@@ -8,17 +8,35 @@ main() {
   fi
 
   local -r github_handle="$1"
-  local -r github_url="https://api.github.com/users/$github_handle/starred?per_page=1000"
 
   # Print CSV Header
-  echo '"repo","url","description"'
+  echo '"repo","starred_at","url","description"'
 
-  # Print rows
-  local page=1
-  while curl -s --fail --show-error "$github_url&page=${page}" \
-    | jq -r -e '.[] | [.full_name, .html_url, .description] | @csv' && [[ ${PIPESTATUS[1]} != 4 ]]; do
-    let page++
-  done
+  # Using GitHub CLI for better pagination.
+  #
+  # [1]: I found out from
+  # https://gist.github.com/jasonrudolph/5abee158b42b99a3990a that using the
+  # correct MIME type of vnd.github.v3.star+json result with the field
+  # starred_at.`
+  #
+  # [2]: By default, GitHub API implicitly responds in reverse chronologically
+  # order, but I don't want to rely on this undocumented behavior. Here I do an
+  # explicit sort.
+  gh api \
+    -X GET "/users/$github_handle/starred" -q 'per-page=1000' \
+    --paginate `# [1]` \
+    --header 'Accept: application/vnd.github.v3.star+json' \
+    --jq '. | sort_by(.starred_at)
+            | reverse 
+            | .[] 
+            | { 
+                  full_name: .repo.full_name,
+                  starred_at: .starred_at,
+                  description: .repo.description,
+                  html_url: .repo.html_url
+              }
+           | [.full_name, .starred_at, .html_url, .description] | @csv' 
+
 }
 
 main "$@"
